bb15b25f2c3756be61c54902ad114ba132f55dad
Harikrishna-Challa <harichvk18@gmail.com>
2025-08-26 18:27:10 +0530

    relocation of project section

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ import './App.css';
 
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import Projects from './components/Projects';
 import About from './components/About';
 import Skills from './components/Skills';
 import Tools from './components/Tools';
 import Education from './components/Education';
 import Certificates from './components/Certificates';
+import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
